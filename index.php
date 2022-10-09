d5a68acee856e5d0b0c6371468e9d5aec4e73a30
pankratov2010 <112240192+pankratov2010@users.noreply.github.com>
2022-10-09 17:33:40 +0300

    date_in_footer

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <section class="services">
+        <div class="container">
+            <h1>Services that connect you to your users</h1>
+            <div class="services-flex">
+                <div class="services-flex__item">
+                    <img src="img/services-icon.svg" alt="Icon">
+                    <h3>Lorem, ipsum dolor.</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a href="#">link</a>
+                </div>
+                <div class="services-flex__item">
+                    <img src="img/services-icon.svg" alt="Icon">
+                    <h3>Lorem, ipsum dolor.</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a href="#">link</a>
+                </div>
+                <div class="services-flex__item">
+                    <img src="img/services-icon.svg" alt="Icon">
+                    <h3>Lorem, ipsum dolor.</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a href="#">link</a>
+                </div>
+                <div class="services-flex__item">
+                    <img src="img/services-icon.svg" alt="Icon">
+                    <h3>Lorem, ipsum dolor.</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a href="#">link</a>
+                </div>
+                <div class="services-flex__item">
+                    <img src="img/services-icon.svg" alt="Icon">
+                    <h3>Lorem, ipsum dolor.</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a href="#">link</a>
+                </div>
+                <div class="services-flex__item">
+                    <img src="img/services-icon.svg" alt="Icon">
+                    <h3>Lorem, ipsum dolor.</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a href="#">link</a>
+                </div>
+            </div>
+        </div>
+
+        <!--button -->
+        <div class="button-flex">
+            <button>Load more</button>
+        </div>
+    </section>
+
+
+
+    <!--<header>
+        <img src="" alt="">
+        <nav>
+            <a href="#">asdasd</a>
+            <a href="#">asdasd</a>
+            <a href="#">asdasd</a>
+            <a href="#">asdasd</a>
+        </nav>
+    </header>
+
+    <main>
+        <section class="features">
+
+        </section>
+
+        <section class="clients">
+
+        </section>
+
+        <section class="clients">
+
+        </section>
+    </main>
+
+    <!-- Подвал -->
+    <footer>
+
+
+        <center>Домашнее задание вывести дату в подвал | <?php echo date('l jS \of F Y h:i:s A'); ?></center>  <!--выводится выровненный по центру текст и функция дата с праметрами в скобках  -->
+
+
+    </footer>
+    <!-- /Окончание подвала /footer -->
+
+</body>
+
+</html>
