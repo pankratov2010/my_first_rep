171af380db3d4fab18e5db16304bd24c19800b79
pankratov2010 <112240192+pankratov2010@users.noreply.github.com>
2022-10-13 22:42:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -95,29 +95,8 @@
 <?php
 
 
-function date_time() {
-$h = date("H");
-$m = date("i");
-if ($h==1 || $h==21) {
-$hours = " час";}
-elseif (($h>=2 && $h<=4) || ($h>=22 && $h<=24)) {
-$hours = " часа";}
-else {$hours = " часов";}
-
-if ($m % 10 === 1 && $m != 11) {
-$minutes = " минута.";}
-elseif (($m % 10 >= 2 && $m % 10 <= 4) && ($m !== 12 && $m !== 13 && $m !== 14)) {
-$minutes = " минуты.";}
-else {
-$minutes = " минут.";}
-$d = date('D M Y год, ');
-$t = $h . $hours . " " . $m . $minutes;
-$array = array($d, $t);
+include 'foo date_time.php';
 echo $d, $t;
-
-
-}
-date_time()
 
 ?>
 
