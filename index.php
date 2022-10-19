72635f6d526555ad66239cd5cc4164dd40ea924d
pankratov2010 <112240192+pankratov2010@users.noreply.github.com>
2022-10-19 23:19:07 +0300

    arrays

--- index.php
+++ index.php
@@ -1,42 +1,38 @@
 <?php
 
-function divisionNumbersInBetween (int $arg1, int $arg2, int $arg3){
+$arrayObl = [
+    'Московская область' => [
+        'Москва',
+        'Зеленоград',
+        'Клин'
+    ],
+    'Ленинградская область' => [
+        'Санкт-Петербург',
+        'Всеволожск',
+        'Павловск',
+        'Кронштадт'
+    ],
+];
 
-    while ($arg1 <= $arg2) {
+echo "<br>";
 
-        if ($arg1 % $arg3 == 0){
-            $array[] = $arg1;
-        }
-        $arg1++;
+foreach ($arrayObl as $obl => $city) {
+    echo $obl . ": <br>";
+    foreach ($city as $cityName) {
+        echo $cityName . "<br>";
     }
-
-return ($array);
-
 }
 
-var_dump (divisionNumbersInBetween (0, 100, 3));
-echo '<br/>';
 
-function evenNotEvenNumbers (){
-
-    $i = 0;
-    do {
-        if ($i == 0){
-            echo $i, ' – это ноль.', '<br/>';
-
-     }
-        elseif ($i % 2 == 0){
-            echo $i, ' – четное число.', '<br/>';
-     }
-        elseif ($i % 2 != 0){
-            echo $i, ' – нечетное число.', '<br/>';
-     }
-        $i++;
-    } while ($i <= 10);
-
+echo "<br>";
+foreach ($arrayObl as $obl => $city) {
+    echo $obl . ": <br>";
+    foreach ($city as $cityName) {
+        if(mb_substr($cityName,0,1) == 'К') {
+            echo $cityName . "<br>";
 }
-
-evenNotEvenNumbers ()
+}
+}
 
 
 ?>
